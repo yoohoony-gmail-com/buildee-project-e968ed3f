e6672dbf02829a018ef5fff69a9ab80875d443a2
yoohoony-gmail-com <yoohoony@gmail.com>
2026-05-02 00:01:40 +0900

    Update vite.config.ts

--- components/vite.config.ts
+++ components/vite.config.ts
@@ -1,9 +1,10 @@
-import path from 'path'
+import tailwindcss from '@tailwindcss/vite'; 
+import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: { alias: { '@': path.resolve(__dirname, './src') } },
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 });
